a7a87d6b756eb59b3c3c96b2877d077b54110aaf
Eugenia Jorge <eugeniajorge95@gmail.com>
2020-09-29 11:05:46 -0300

    probando jajja

--- templates_c/02076021b8fcf8fa1edff5e2fd5694905e1feddf_0.file.servicios.tpl.php
+++ templates_c/02076021b8fcf8fa1edff5e2fd5694905e1feddf_0.file.servicios.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-09-28 14:56:04
+/* Smarty version 3.1.34-dev-7, created on 2020-09-29 00:34:27
   from 'D:\Users\Usuario\AppData\Local\Programs\xampp\htdocs\web2-2020\templates\servicios.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5f71dd6482af60_48987458',
+  'unifunc' => 'content_5f7264f3ee6fa7_39802686',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '02076021b8fcf8fa1edff5e2fd5694905e1feddf' => 
     array (
       0 => 'D:\\Users\\Usuario\\AppData\\Local\\Programs\\xampp\\htdocs\\web2-2020\\templates\\servicios.tpl',
-      1 => 1601297751,
+      1 => 1601332435,
       2 => 'file',
     ),
   ),
@@ -23,7 +23,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_5f71dd6482af60_48987458 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5f7264f3ee6fa7_39802686 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:navbar.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
@@ -31,7 +31,8 @@ $_smarty_tpl->_subTemplateRender("file:navbar.tpl", $_smarty_tpl->cache_id, $_sm
 <section class="servicios">
         
 <div class="filtrar">
-    <h5>Filtrar por categoria</h5>
+<form>
+<button type="button">Filtrar por categoria</button>
     <select name="filtro" class="filtroCategoria">
     <option value=" "> </option>
     <?php
@@ -47,6 +48,7 @@ $_smarty_tpl->tpl_vars['categoria']->do_else = false;
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
     </select>
+    </form>
 </div>
 
 <?php
